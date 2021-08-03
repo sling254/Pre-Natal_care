ec40f11664b3a5d71ac38e4889764322aea10f88
sling254 <mwanikidenis7@gmail.com>
2021-08-03 15:42:48 +0300

    add and style the record page

--- Pre_Natal_care/maternal_care/urls.py
+++ Pre_Natal_care/maternal_care/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, record
 
 urlpatterns = [
     path('', index, name='index'),
+    path('record/', record, name='record'),
     
 ]
